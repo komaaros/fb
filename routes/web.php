a75bb1adcbf33b40fbde19befad9dfd52a725959
marko <mare.mladenovic@gmail.com>
2019-03-04 10:39:10 +0100

    a

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('admlogin')->group(function () {
+    Route::get('index', 'Admlogin\AdmloginController@index')->name('Admlogin_index');
+    Route::post('index/create', 'Admlogin\AdmloginController@create')->name('Admlogin_create');
+    Route::put('index/edit', 'Admlogin\AdmloginController@edit')->name('Admlogin_edit');
+    Route::delete('index/delete', 'Admlogin\AdmloginController@delete')->name('Admlogin_delete');
 });
+
